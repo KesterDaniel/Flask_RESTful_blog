af2b95fb672df11bdeafcacabeda44cadb4960a3
KesterDaniel <kesterdan17@gmail.com>
2023-04-12 18:37:06 +0100

    reading posts

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
+import datetime as dt
+
 
 
 app = Flask(__name__)
@@ -48,8 +51,9 @@ def get_all_posts():
 @app.route("/post/<int:index>")
 def show_post(index):
     requested_post = None
+    posts = db.session.query(BlogPost).all()
     for blog_post in posts:
-        if blog_post["id"] == index:
+        if blog_post.id == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
 
@@ -63,10 +67,27 @@ def about():
 def contact():
     return render_template("contact.html")
 
-@app.route("/newpost")
+@app.route("/newpost", methods=["GET", "POST"])
 def new_post():
     new_post_form = CreatePostForm()
+    if new_post_form.validate_on_submit():
+        new_blog_post = BlogPost(
+            title = new_post_form.title.data,
+            subtitle = new_post_form.subtitle.data,
+            author = new_post_form.author.data,
+            date = dt.datetime.now().date().strftime("%B %d, %Y"),
+            img_url = new_post_form.img_url.data,
+            body = new_post_form.body.data
+        )
+        db.session.add(new_blog_post)
+        db.session.commit()
+        return redirect(url_for("get_all_posts"))
     return render_template("make-post.html", form=new_post_form)
+
+
+@app.route("/editpost/<int:post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    return "ME EDIT"
 
 if __name__ == "__main__":
     db.create_all()
